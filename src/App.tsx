e4a81cced2b4755d4d7981e4ed0436b96d6f688f
colki <hayun.yang@thingsflow.com>
2024-03-11 19:17:16 +0900

    feat: emotion세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { Global, css } from '@emotion/react';
+import emotionNormalize from 'emotion-normalize';
 import Routes from 'pages/Routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Global styles={globalStyles} />
       <Routes />
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const globalStyles = css`
+  ${emotionNormalize}
+  * {
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    margin: 0;
+  }
+`;
