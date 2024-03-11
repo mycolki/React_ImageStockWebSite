0c2f388c9ec182e6b7eb1aeb6ef3312f5f9274d5
colki <hayun.yang@thingsflow.com>
2024-03-11 22:02:40 +0900

    feat: react-query세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import { BrowserRouter } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+  keepPreviousData,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Global, css } from '@emotion/react';
 import emotionNormalize from 'emotion-normalize';
 import { vars } from 'style/vars';
 import Routes from 'pages/Routes';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchIntervalInBackground: false,
+      retry: false,
+      placeholderData: keepPreviousData,
+      refetchOnWindowFocus: false,
+      throwOnError: true,
+    },
+  },
+});
+
 function App() {
   return (
     <BrowserRouter>
       <Global styles={globalStyles} />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
